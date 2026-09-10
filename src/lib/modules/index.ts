af021c87340d5cc4034b1682a9fd81c1881680d5
Profgabby <ayayiagabriel2020@gmail.com>
2026-09-10 14:44:18 -0700

    Register authored PF-06 curriculum module

--- src/lib/modules/index.ts
+++ src/lib/modules/index.ts
@@ -6,6 +6,7 @@ import { getPF02 } from "@/lib/modules/pathways-foundation/pf-02";
 import { getPF03 } from "@/lib/modules/pathways-foundation/pf-03";
 import { getPF04 } from "@/lib/modules/pathways-foundation/pf-04";
 import { getPF05 } from "@/lib/modules/pathways-foundation/pf-05";
+import { getPF06 } from "@/lib/modules/pathways-foundation/pf-06";
 import { generateFullModule } from "@/lib/modules/generated-module";
 
 export type FullCurriculumModule = PF01Module | (Omit<PF01Module,"code"> & { code:string });
@@ -16,6 +17,7 @@ const authoredModuleGetters: Record<string,(language:AppLanguage)=>FullCurriculu
   "PF-03": getPF03,
   "PF-04": getPF04,
   "PF-05": getPF05,
+  "PF-06": getPF06,
 };
 
 export const developedModuleCodes = Object.freeze(curriculumPrograms.flatMap(program=>program.lessons.map(lesson=>lesson.code)));
