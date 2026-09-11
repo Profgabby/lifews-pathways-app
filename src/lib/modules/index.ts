dfc18985978042b252747d920b7f6f69d0c19eec
Profgabby <ayayiagabriel2020@gmail.com>
2026-09-10 17:16:34 -0700

    Register full PE-05 Measuring the Garden module

--- src/lib/modules/index.ts
+++ src/lib/modules/index.ts
@@ -17,6 +17,7 @@ import { getPE01 } from "@/lib/modules/pathways-explorer/pe-01";
 import { getPE02 } from "@/lib/modules/pathways-explorer/pe-02";
 import { getPE03 } from "@/lib/modules/pathways-explorer/pe-03";
 import { getPE04 } from "@/lib/modules/pathways-explorer/pe-04";
+import { getPE05 } from "@/lib/modules/pathways-explorer/pe-05";
 import { generateFullModule } from "@/lib/modules/generated-module";
 
 export type FullCurriculumModule = PF01Module | (Omit<PF01Module,"code"> & { code:string });
@@ -38,6 +39,7 @@ const authoredModuleGetters: Record<string,(language:AppLanguage)=>FullCurriculu
   "PE-02": getPE02,
   "PE-03": getPE03,
   "PE-04": getPE04,
+  "PE-05": getPE05,
 };
 
 export const developedModuleCodes = Object.freeze(curriculumPrograms.flatMap(program=>program.lessons.map(lesson=>lesson.code)));
